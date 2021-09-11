32fd3197c634624cd8ffbf9278c611b80cc3a1db
Daniel Coker <danielcoker28@outlook.com>
2021-09-11 02:04:21 +0100

    Add environment variables

--- movies_api/settings.py
+++ movies_api/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+# Setup environment variables
+env = environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^0z@3_8%=s_ufyb)!bng5s@f@k7_6r=szrbp*j9@zktv!750rw'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,11 +82,11 @@ WSGI_APPLICATION = 'movies_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'movies_db',
-        'USER': 'danielcoker',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
         'PASSWORD': '',
-        'HOST': 'localhost',
-        'POST': '5432'
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT')
     }
 }
 
