8822160b28d92a03bbdd3c0af62a1d8e792ae2a3
Daniel Coker <danielcoker28@outlook.com>
2021-09-14 16:23:20 +0100

    Add movie model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    year = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    creator = models.ForeignKey(
+        User, related_name='movies', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-id']
