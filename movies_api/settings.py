62133b2f67f00304290a41d5889e02cb1f65422a
Daniel Coker <danielcoker28@outlook.com>
2021-09-14 16:24:22 +0100

    Remove auth from installed apps

--- movies_api/settings.py
+++ movies_api/settings.py
@@ -44,8 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken'
-    'auth',
+    'rest_framework.authtoken',
     'movies',
 ]
 
