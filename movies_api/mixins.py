ed76e4627c9e1e9054dcf201e577ad8997bf88c8
Daniel Coker <danielcoker28@outlook.com>
2021-09-21 10:02:29 +0100

    Handle resource name when queryset is empty

--- movies_api/mixins.py
+++ movies_api/mixins.py
@@ -8,7 +8,13 @@ class SuccessMessageMixin:
 
     def get_renderer_context(self):
         context = super().get_renderer_context()
-        context['resource_name'] = type(self.get_queryset()[0]).__name__
+
+        try:
+            context['resource_name'] = type(self.get_queryset()[0]).__name__ if hasattr(
+                self, 'get_queryset') else self.resource_name
+        except IndexError as e:
+            context['success_message'] = self.success_message = 'No records found.'
+
         context['success_message'] = self.success_message
 
         return context
